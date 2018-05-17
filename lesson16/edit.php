994b65b92d721abbad4866a91971ff7aa1b1f350
Geraldas <38309969+GeraldasK@users.noreply.github.com>
2018-05-18 00:40:07 +0300

    Update edit.php

--- lesson16/edit.php
+++ lesson16/edit.php
@@ -1,19 +1,14 @@
 <?php
+session_start();
 require "vendor/autoload.php";
 use Chuck\Chuck;
 use Chuck\Data\db;
 
-//$chuck = new Chuck;
-//$chuck->editChuck();
-
-//var_dump($_POST);
 if(isset($_GET['edit'])):
   $id = $_GET['edit'];
-  $sql = "SELECT icon_url, url , value FROM chuck WHERE id = '$id'";
-  $db = new Db;
-  $sth = $db->getDb()->prepare($sql);
-  $sth->execute();
-  $record = $sth->fetch(PDO::FETCH_ASSOC);    
+  $edit = new Chuck;
+  $record = $edit->editChuck($id);  
+  
 endif;    
 
 if(isset($_POST['update'])){
@@ -22,18 +17,12 @@ if(isset($_POST['update'])){
   $id = $_POST['id'];
   $icon_url = $_POST['icon_url'];
   $value = $_POST['value'];
-  var_dump($url);
-  /*$sql = '
-  UPDATE chuck SET icon_url = "'.$icon_url.'", url = "'.$url.'", value = "'.$value.'"
-  WHERE id ="'.$id.'"';*/
   $sql = '
   UPDATE chuck SET icon_url = "'.$icon_url.'", url = "'.$url.'", value = "'.$value.'"
   WHERE id ="'.$id.'"';
-  //var_dump($sql);
-  $sth = $db->getDb()->prepare($sql);
-  $sth->execute();
-  echo $sth->rowCount() . " records UPDATED successfully";
-
+  $sth = $db->query($sql);
+  $_SESSION['message'] = "Record updated!"; 
+	header('location: index.php');
 }
 
 ?>
@@ -50,15 +39,10 @@ if(isset($_POST['update'])){
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   </head>
   <body class="container">
-        <form action="" method="POST">
-            <?php /* if(isset($_POST['id'])):
-              $id = $_POST['id'];
-              $sql = "SELECT * FROM chuck WHERE id LIKE '$id'";
-                $persons = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-                foreach($persons as $person);
-                //var_dump($person);*/?>
-      
+  <!---Forma-->
+      <form action="" method="POST">
         <div class="form-group">
+
             URL<input type="text" class="form-control" name="url" value="<?php echo $record['url']?>">
             Icon_url<input type="text" class="form-control" name="icon_url" value="<?php echo $record['icon_url']?>">
             value<input type="text" class="form-control" name="value" value="<?php echo $record['value']?>">
@@ -66,8 +50,9 @@ if(isset($_POST['update'])){
             <input type="hidden" name="id" value="<?php echo $id?>">
             <input type="submit" class="btn btn-primary" name="update" value="update">
             
-            </div>
-        </form>
+        </div>
+      </form>
+  <!---Forma-->
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
